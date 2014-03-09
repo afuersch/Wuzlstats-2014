9b3bcd9bc17d5205449af993c10bb31a643c6b9e
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2014-03-09 19:48:49 +0100

    "Players" page.

---
+++ WuzlStats/Controllers/PlayersController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using WuzlStats.Models;
+using WuzlStats.ViewModels.Players;
+
+namespace WuzlStats.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private readonly Db _db = new Db();
+
+        public ActionResult Index()
+        {
+            var viewModel = new IndexViewModel().Build(_db);
+            return View(viewModel);
+        }
+	}
+}
---
+++ WuzlStats/ViewModels/Players/IndexViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WuzlStats.Models;
+
+namespace WuzlStats.ViewModels.Players
+{
+    public class IndexViewModel
+    {
+        public IndexViewModel Build(Db db)
+        {
+            var playerNames = (db.Scores.Select(x => x.TeamBlueOffensePlayer)
+                .Concat(db.Scores.Select(x => x.TeamBlueDefensePlayer))
+                .Concat(db.Scores.Select(x => x.TeamRedOffensePlayer))
+                .Concat(db.Scores.Select(x => x.TeamRedDefensePlayer))).Distinct().OrderBy(x => x).ToList();
+
+            Players = (from playerName in playerNames
+                       let playerScores =
+                           db.Scores.Where(x => (x.TeamBlueDefensePlayer == playerName || x.TeamBlueOffensePlayer == playerName || x.TeamRedDefensePlayer == playerName || x.TeamRedOffensePlayer == playerName) && x.TeamBlueDefensePlayer != x.TeamBlueOffensePlayer && x.TeamRedDefensePlayer != x.TeamRedOffensePlayer)
+                       select new Player
+                       {
+                           Name = playerName,
+                           LastPlayedDate = playerScores.Max(x => x.Date),
+                           NumberOfGames = playerScores.Count()
+                       }).ToList();
+
+            return this;
+        }
+
+
+        public IEnumerable<Player> Players { get; set; }
+
+        public class Player
+        {
+            public string Name { get; set; }
+            public DateTime LastPlayedDate { get; set; }
+            public int NumberOfGames { get; set; }
+        }
+    }
+}
