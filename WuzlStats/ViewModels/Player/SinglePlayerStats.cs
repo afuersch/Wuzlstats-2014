d2a8f572cef21ee345bf15bf015c0f698f6da50d
afuersch <adrian.fuerschuss@gmx.net>
2014-09-02 19:32:57 +0200

    Add single player stats class.

---
+++ WuzlStats/ViewModels/Player/SinglePlayerStats.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WuzlStats.ViewModels.Player
+{
+    public class SinglePlayerStats : IPlayerStats
+    {
+        public int WinsCount { get; set; }
+        public int LossesCount { get; set; }
+        public string FavoriteTeam { get; set; }
+    }
+}
