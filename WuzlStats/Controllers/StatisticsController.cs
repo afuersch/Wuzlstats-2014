0f87c54c5c0a817eae234aab1b2ec8a7de6bc8ea
afuersch <adrian.fuerschuss@gmx.net>
2014-09-26 15:57:51 +0200

    #3: Add statistics controller and view.

---
+++ WuzlStats/Controllers/StatisticsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WuzlStats.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
