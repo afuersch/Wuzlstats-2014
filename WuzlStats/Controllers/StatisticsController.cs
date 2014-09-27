28b8307d66ba92f2cad328e5dc8c81a38b0733b7
afuersch <adrian.fuerschuss@gmx.net>
2014-09-27 10:35:49 +0200

    #3: Implement Statistics controller index action.

--- WuzlStats/Controllers/StatisticsController.cs
+++ WuzlStats/Controllers/StatisticsController.cs
@@ -1,12 +1,31 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using WuzlStats.Models;
+using WuzlStats.ViewModels.Statistics;
 
 namespace WuzlStats.Controllers
 {
     public class StatisticsController : Controller
     {
+        private readonly Db _db = new Db();
+
         public ActionResult Index()
         {
-            return View();
+            //build model
+            var players = from player in _db.Players
+                          select new SelectListItem
+                          {
+                              Text = player.Name,
+                              Value = player.Name
+                          };
+
+            IndexViewModel model = new IndexViewModel
+            {
+                End = DateTime.Today,
+                Players = players
+            };
+            return View(model);
         }
     }
 }
