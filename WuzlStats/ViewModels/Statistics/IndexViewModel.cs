64530c23c985926b16c9e3c4f2483fc4e6fef860
afuersch <adrian.fuerschuss@gmx.net>
2014-09-27 10:23:53 +0200

    #3: Add input properties to statistics view model.

--- WuzlStats/ViewModels/Statistics/IndexViewModel.cs
+++ WuzlStats/ViewModels/Statistics/IndexViewModel.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using WuzlStats.Models;
 
 namespace WuzlStats.ViewModels.Statistics
 {
     public class IndexViewModel
     {
-        public IEnumerable<string> Players { get; set; }
         public DateTime? Start { get; set; }
         public DateTime? End { get; set; }
+        public IEnumerable<SelectListItem> Players { get; set; }
+        public Team? Team1Color { get; set; }
+        public Team? Team2Color { get; set; }
+        public string Team1Player1 { get; set; }
+        public string Team1Player2 { get; set; }
+        public string Team2Player1 { get; set; }
+        public string Team2Player2 { get; set; }
     }
 }
